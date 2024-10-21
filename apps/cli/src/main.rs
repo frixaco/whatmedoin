e82b1f93760f9938863b1d211d37134c002e7f91
frixaco <rustam21ashurmatov@gmail.com>
2024-10-21 15:02:48 +0500

    Update .gitignore; Set up CLI app

--- apps/cli/src/main.rs
+++ apps/cli/src/main.rs
@@ -1,3 +1,12 @@
+use x_win::{get_active_window, XWinError};
+
 fn main() {
-    println!("Hello, world!");
+    match get_active_window() {
+        Ok(active_window) => {
+            println!("active window: {:#?}", active_window);
+        }
+        Err(XWinError) => {
+            println!("error occurred while getting the active window");
+        }
+    }
 }
