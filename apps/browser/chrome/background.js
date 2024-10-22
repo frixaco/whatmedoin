3e253f05d53a9401d4156e5b0895701a6c251421
frixaco <rustam21ashurmatov@gmail.com>
2024-10-22 13:38:26 +0500

    Cleanup browser ext code

--- apps/browser/chrome/background.js
+++
@@ -1,7 +0,0 @@
-// chrome/background.ts
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Extension installed");
-});
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log(tab.url, tab.title, tab.id, tabId);
-});
--- apps/browser/firefox/background.js
+++
@@ -1,7 +0,0 @@
-browser.runtime.onInstalled.addListener(() => {
-  console.log("Extension installed");
-});
-
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log(tab.url, tab.title, tab.id, tabId);
-});
