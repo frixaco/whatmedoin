136bfa7fa88aab40feb847405dc8137051720f3f
frixaco <rustam21ashurmatov@gmail.com>
2024-10-22 14:14:36 +0500

    Make browser extensions work

--- apps/browser/firefox/background.ts
+++ apps/browser/firefox/background.ts
@@ -1,7 +1,18 @@
+const API_URL = "http://localhost:3000";
+
 browser.runtime.onInstalled.addListener(() => {
-  console.log("Extension installed");
+  console.log("whatmedoin extension installed");
 });
 
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log(tab.url, tab.title, tab.id, tabId);
+browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (changeInfo.status == "complete" && tab.active) {
+    const response = await fetch(API_URL + "/new-event", {
+      method: "POST",
+      body: JSON.stringify({
+        type: "browser_tab",
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+  }
 });
