d8ed51f194840a9fb7edeed74e228521abffb2f9
frixaco <rustam21ashurmatov@gmail.com>
2024-10-24 00:58:35 +0500

    Firefox ext should work

--- apps/browser/firefox/popup.ts
+++ apps/browser/firefox/popup.ts
@@ -1,22 +1,71 @@
-function getTitle() {
-  browser.tabs
-    .query({ active: true, currentWindow: true })
-    .then((tabs) => {
-      const activeTab = tabs[0];
-      console.log(activeTab.title, activeTab.url);
+function getActiveTab() {
+  browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+    const activeTab = tabs[0];
 
-      const tabTitle = document.getElementById("tab-title");
-      if (tabTitle) {
-        tabTitle.innerText = activeTab.title || "No title found";
-      } else {
-        console.error("No tab title element found");
-      }
-    })
-    .catch((error) => {
-      console.error("Error getting tab:", error);
+    let tabTitle = "...";
+    let tabUrl = "...";
+
+    const tabTitleEl = document.getElementById("active-tab-title");
+    if (tabTitleEl) {
+      tabTitleEl.innerText = activeTab.title || "Could not get title";
+      tabTitle = activeTab.title || "Could not get title";
+    } else {
+      console.error("No tab title element found");
+    }
+
+    const tabUrlEl = document.getElementById("active-tab-url");
+    if (tabUrlEl) {
+      tabUrlEl.innerText = activeTab.url || "Could not get url";
+      tabUrl = activeTab.url || "Could not get url";
+    } else {
+      console.error("No tab url element found");
+    }
+
+    browser.runtime.sendMessage({
+      type: "active-tab-info",
+      title: tabTitle,
+      url: tabUrl,
     });
+  });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("get-title")?.addEventListener("click", getTitle);
+  document.getElementById("refresh")?.addEventListener("click", getActiveTab);
+
+  document
+    .getElementById("set-api-url")
+    ?.addEventListener("click", async () => {
+      if (
+        (document.getElementById("api-url") as HTMLInputElement).value === ""
+      ) {
+        document.getElementById("set-api-url-status")!.innerText =
+          "Provide a URL";
+        document.getElementById("set-api-url-status")!.style.color = "white";
+        return;
+      }
+
+      document.getElementById("set-api-url-status")!.innerText = "Setting...";
+
+      const apiUrl = (document.getElementById("api-url") as HTMLInputElement)
+        .value;
+
+      browser.storage.session.set({ apiUrl });
+      // browser.runtime.sendMessage({
+      //   type: "api-url",
+      //   url: apiUrl,
+      // });
+
+      const storageValueCheck = await browser.storage.session.get("apiUrl");
+      if (storageValueCheck) {
+        document.getElementById("set-api-url-status")!.innerText = "Success";
+        // make text green and remove text after 2 seconds
+        document.getElementById("set-api-url-status")!.style.color = "green";
+      } else {
+        document.getElementById("set-api-url-status")!.innerText = "Failed";
+        document.getElementById("set-api-url-status")!.style.color = "red";
+      }
+      setTimeout(() => {
+        document.getElementById("set-api-url-status")!.innerText = "";
+      }, 2000);
+    });
 });
