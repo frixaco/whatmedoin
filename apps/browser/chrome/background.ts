6f33ec46c6682b899a539e2005e34ed75716e6a8
frixaco <rustam21ashurmatov@gmail.com>
2024-10-23 23:59:34 +0500

    Make Chrome extension work; Add styles

--- apps/browser/chrome/background.ts
+++ apps/browser/chrome/background.ts
@@ -1,24 +1,40 @@
-const API_URL = "http://localhost:3000";
-
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("whatmedoin extension installed");
-});
+// let API_URL = "http://localhost:3000";
 
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
   if (changeInfo.status == "complete" && tab.active) {
-    const response = await fetch(API_URL + "/new-event", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        type: "browser_tab",
-        title: tab.title,
-        url: tab.url,
-        app_name: navigator.userAgent,
-      }),
-    });
-    const data = await response.json();
-    console.log(data);
+    console.log("🔵 whatmedoin processing active tab");
+    try {
+      const sessionValue = await chrome.storage.session.get("apiUrl");
+      const apiUrl = sessionValue.key;
+      if (!apiUrl || apiUrl === "") {
+        console.warn("🟡 whatmedoin: No API URL provided");
+        return;
+      }
+      const response = await fetch(apiUrl + "/activity", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          platform: "browser",
+          title: tab.title,
+          url: tab.url,
+        }),
+      });
+      if (response.ok) {
+        console.log("🟢 whatmedoin");
+      } else {
+        console.error("🔴 whatmedoin API error:", response.statusText);
+      }
+    } catch (error) {
+      console.error("🔴 whatmedoin extension error:", error);
+    }
   }
 });
+
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message.type === "api-url") {
+//     API_URL = message.url;
+//   }
+//   return true;
+// });
--- apps/browser/chrome/popup.ts
+++ apps/browser/chrome/popup.ts
@@ -1,17 +1,71 @@
-function getTitle() {
+function getActiveTab() {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const activeTab = tabs[0];
-    console.log(activeTab.title, activeTab.url);
 
-    const tabTitle = document.getElementById("tab-title");
-    if (tabTitle) {
-      tabTitle.innerText = activeTab.title || "No title found";
+    let tabTitle = "...";
+    let tabUrl = "...";
+
+    const tabTitleEl = document.getElementById("active-tab-title");
+    if (tabTitleEl) {
+      tabTitleEl.innerText = activeTab.title || "Could not get title";
+      tabTitle = activeTab.title || "Could not get title";
     } else {
       console.error("No tab title element found");
     }
+
+    const tabUrlEl = document.getElementById("active-tab-url");
+    if (tabUrlEl) {
+      tabUrlEl.innerText = activeTab.url || "Could not get url";
+      tabUrl = activeTab.url || "Could not get url";
+    } else {
+      console.error("No tab url element found");
+    }
+
+    chrome.runtime.sendMessage({
+      type: "active-tab-info",
+      title: tabTitle,
+      url: tabUrl,
+    });
   });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("get-title")?.addEventListener("click", getTitle);
+  document.getElementById("refresh")?.addEventListener("click", getActiveTab);
+
+  document
+    .getElementById("set-api-url")
+    ?.addEventListener("click", async () => {
+      if (
+        (document.getElementById("api-url") as HTMLInputElement).value === ""
+      ) {
+        document.getElementById("set-api-url-status")!.innerText =
+          "Provide a URL";
+        document.getElementById("set-api-url-status")!.style.color = "white";
+        return;
+      }
+
+      document.getElementById("set-api-url-status")!.innerText = "Setting...";
+
+      const apiUrl = (document.getElementById("api-url") as HTMLInputElement)
+        .value;
+
+      chrome.storage.session.set({ apiUrl });
+      // chrome.runtime.sendMessage({
+      //   type: "api-url",
+      //   url: apiUrl,
+      // });
+
+      const storageValueCheck = await chrome.storage.session.get("apiUrl");
+      if (storageValueCheck) {
+        document.getElementById("set-api-url-status")!.innerText = "Success";
+        // make text green and remove text after 2 seconds
+        document.getElementById("set-api-url-status")!.style.color = "green";
+      } else {
+        document.getElementById("set-api-url-status")!.innerText = "Failed";
+        document.getElementById("set-api-url-status")!.style.color = "red";
+      }
+      setTimeout(() => {
+        document.getElementById("set-api-url-status")!.innerText = "";
+      }, 2000);
+    });
 });
