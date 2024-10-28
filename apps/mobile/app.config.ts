64f1932c0b0a3ff832cb915ca258519309d8d446
frixaco <rustam21ashurmatov@gmail.com>
2024-10-28 20:17:03 +0500

    Trying React Native (Expo) for mobile

---
+++ apps/mobile/app.config.ts
@@ -0,0 +1,33 @@
+export default {
+  name: "whatmedoin",
+  slug: "whatmedoin",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "whatmedoin",
+  userInterfaceStyle: "automatic",
+  splash: {
+    image: "./assets/images/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    package: "com.frixaco.whatmedoin",
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+  plugins: ["expo-router"],
+  experiments: {
+    typedRoutes: true,
+  },
+};
